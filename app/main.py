c023ee2dfb7f81f3cd5962d1c0140d9bfaae0b7c
Joshua <joshua.ooi@huawei.com>
2025-11-27 02:22:38 +0800

    edited cors policy 5

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Request
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from uuid import uuid4
